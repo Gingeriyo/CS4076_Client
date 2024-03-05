78620519416f5175c767b735a3ebd2659d2b1e03
Gingeriyo <gingeroshea443@gmail.com>
2024-03-05 15:31:15 +0000

    Revert "file struct and basic javafx"
    This reverts commit aa0e5923cd5e4d0a63c26e33ec8009a4f1bb65b9.

--- src/22342761_22348069_Client/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.cs4076_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.cs4076_project to javafx.fxml;
-    exports org.example.cs4076_project;
-}
