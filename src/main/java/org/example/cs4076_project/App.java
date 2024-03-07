2b7c848672afa4546a5818c0791f6109ddfdb088
Gingeriyo <gingeroshea443@gmail.com>
2024-03-07 00:02:27 +0000

    Startup Page
    buttons dont work right now but the startup page is done unless we need to make it look better.

--- src/main/java/org/example/cs4076_project/App.java
+++ src/main/java/org/example/cs4076_project/App.java
@@ -13,27 +13,51 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
-        private final int minWidth = 720;
-        private final int minHeight = 480;
+    private final int minWidth = 720;
+    private final int minHeight = 480;
+    public Stage stage;
 
-        // setting primary stage
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            primaryStage.setTitle("Client");
-            primaryStage.setResizable(false);
-            primaryStage.setScene(startup());
-            primaryStage.show();
-        }
-
-        private Scene startup() throws IOException {
-            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("StartupView.fxml"));
-            Label lbl = new Label();
-            Button btn = new Button();
-            return new Scene(fxmlLoader.load(), minWidth, minHeight);
-        }
-
-        public static void main(String[] args) {
-            launch();
-        }
-
+    // setting primary stage
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        primaryStage.setTitle("Class Scheduler");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(startup());
+        primaryStage.show();
     }
+
+    //stage.setScene(facultyLogin())
+
+    // Add class
+    // Remove class
+    // View schedule
+    public Scene startup() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("StartupView.fxml"));
+
+        return new Scene(fxmlLoader.load(), minWidth, minHeight);
+    }
+
+    public Scene addClass() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("addScene.fxml"));
+
+        return new Scene(fxmlLoader.load(), minWidth, minHeight);
+    }
+
+    public Scene removeClass() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("addScene.fxml"));
+
+        return new Scene(fxmlLoader.load(), minWidth, minHeight);
+    }
+
+    public Scene viewSchedule() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("viewScheduleView.fxml"));
+
+        return new Scene(fxmlLoader.load(), minWidth, minHeight);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
--- src/main/java/org/example/cs4076_project/Controller.java
+++ src/main/java/org/example/cs4076_project/Controller.java
@@ -1,14 +1,31 @@
 package org.example.cs4076_project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
     @FXML
-    public Label lbl;
+    public Button add;
+    public Button rem;
+    public Button view;
+    public Label title;
+    private final Stage stage = new Stage();
+    private final App app = new App();
 
     @FXML
-    protected void onBtnClick() {
-        lbl.setText("Welcome!");
+    protected void onAdd() throws IOException {
+        stage.setScene(app.addClass());
+    }
+    @FXML
+    protected void onRem() throws IOException {
+        stage.setScene(app.removeClass());
+    }
+    @FXML
+    protected void onView() throws IOException {
+        stage.setScene(app.viewSchedule());
     }
 }
