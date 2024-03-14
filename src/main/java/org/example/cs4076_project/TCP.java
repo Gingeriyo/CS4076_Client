9aff5836a0db8939efe1fe422c9992da11c51108
Buggy <124364531+evanbuggy@users.noreply.github.com>
2024-03-14 01:54:10 +0000

    TCP Class and TCP Tests

---
+++ src/main/java/org/example/cs4076_project/TCP.java
@@ -0,0 +1,50 @@
+package org.example.cs4076_project;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TCP {
+    private static int PORT = 9999;
+    private static InetAddress IP;
+    Socket link = null;
+    BufferedReader in;
+    PrintWriter out;
+
+    public boolean init() {
+        try {
+            IP = InetAddress.getLocalHost();
+        }
+        catch(UnknownHostException e) {
+            System.out.println("IP of local machine not found!");
+            return false;
+        }
+
+        try {
+            link = new Socket(IP,PORT);
+            in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+            out = new PrintWriter(link.getOutputStream(),true);
+        }
+        catch(IOException e)
+        {
+            System.out.println("Unable to establish socket to server!");
+            return false;
+        }
+        return true;
+    }
+
+    public String send(String message) {
+        String response = "";
+        out.println(message);
+        try {
+            response = in.readLine();
+        } catch (IOException e) {
+            return "ERR: Response from server was not received!";
+        }
+        return response;
+    }
+}
---
+++ src/test/java/org/example/cs4076_project/TCPTest.java
@@ -0,0 +1,112 @@
+package org.example.cs4076_project;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TCPTest {
+    @Test
+    @DisplayName("Initialise TCP Client")
+    void initTest() {
+        TCP test = new TCP();
+        // This test will only return true if the server is online
+        assertTrue(test.init());
+    }
+
+    @Test
+    @DisplayName("Send Stop")
+    void sendStop() {
+        TCP test = new TCP();
+        test.init();
+        assertEquals("TERMINATE", test.send("STOP"));
+    }
+
+    @Test
+    @DisplayName("Send Add Success")
+    void sendAddSuccess() {
+        TCP test = new TCP();
+        test.init();
+        assertEquals("ADD_CLASS_SUCCESS", test.send("ADD_CS2004_S205_2024-03-14_10"));
+    }
+
+    @Test
+    @DisplayName("Send Add Incorrect Date")
+    void sendAddIncorrectDate() {
+        TCP test = new TCP();
+        test.init();
+        assertEquals("ERR: Invalid date!", test.send("ADD_CS2004_S205_LOL_10"));
+    }
+
+    @Test
+    @DisplayName("Send Add Incorrect Time")
+    void sendAddIncorrectTime() {
+        TCP test = new TCP();
+        test.init();
+        assertEquals("ERR: Invalid time slot!", test.send("ADD_CS2004_S205_2024-04-05_8"));
+    }
+
+    @Test
+    @DisplayName("Send Add Not enough args")
+    void sendAddNotEnoughArgs() {
+        TCP test = new TCP();
+        test.init();
+        assertEquals("ERR: Not enough arguments!", test.send("ADD"));
+    }
+
+    @Test
+    @DisplayName("Send Add Duplicate")
+    void sendAddDuplicate() {
+        TCP test = new TCP();
+        test.init();
+        test.send("ADD_DUPECLASS_S205_2024-03-14_10");
+        assertEquals("ADD_CLASS_FAIL", test.send("ADD_SOME-OTHER-CLASS_S205_2024-03-14_10"));
+    }
+
+    @Test
+    @DisplayName("Send Add and Remove")
+    void sendAddAndRemove() {
+        TCP test = new TCP();
+        test.init();
+        test.send("ADD_TO-BE-REMOVED_STUPID-ROOM_2024-03-15_10");
+        assertEquals("REMOVE_CLASS_SUCCESS", test.send("REMOVE_TO-BE-REMOVED_STUPID-ROOM_2024-03-15_10"));
+    }
+
+    @Test
+    @DisplayName("View Added Classes")
+    void viewAddedClasses() {
+        TCP test = new TCP();
+        test.init();
+        test.send("ADD_777_BACKROOMS_2024-03-16_17");
+        test.send("ADD_COOL-CLASS_BACKROOMS_2024-03-16_11");
+        test.send("ADD_SUPER-DUPER-CLASS_BACKROOMS_2024-03-16_9");
+        assertEquals("17_777_BACKROOMS~9_SUPER-DUPER-CLASS_BACKROOMS~11_COOL-CLASS_BACKROOMS~", test.send("VIEW_2024-03-16"));
+    }
+
+    @Test
+    @DisplayName("View Added Classes by Name")
+    void viewAddedClassesNamed() {
+        TCP test = new TCP();
+        test.init();
+        test.send("ADD_777_BACKROOMS_2024-03-16_17");
+        test.send("ADD_COOL-CLASS_BACKROOMS_2024-03-16_11");
+        test.send("ADD_SUPER-DUPER-CLASS_BACKROOMS_2024-03-16_9");
+        assertEquals("11_COOL-CLASS_BACKROOMS~", test.send("VIEWCLASS_2024-03-16_COOL-CLASS"));
+    }
+
+    @Test
+    @DisplayName("View Added Classes by Name Same Day")
+    void viewAddedClassesNamedSameDay() {
+        TCP test = new TCP();
+        test.init();
+        test.send("ADD_777_kemmy_2024-03-16_17");
+        test.send("ADD_COOL-CLASS_BACKROOMS_2024-03-16_11");
+        test.send("ADD_777_BACKROOMS_2024-03-16_9");
+        test.send("ADD_777_The-Garage_2024-03-16_9");
+        assertEquals("17_777_kemmy~9_777_BACKROOMS~9_777_The-Garage~", test.send("VIEWCLASS_2024-03-16_777"));
+    }
+}
