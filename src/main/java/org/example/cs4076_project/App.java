314ea1a3d552d87c4943d43488c1359a6d93b85f
VenturiVen <gingeroshea443@gmail.com>
2024-04-19 16:38:20 +0100

    test

--- src/main/java/org/example/cs4076_project/App.java
+++ src/main/java/org/example/cs4076_project/App.java
@@ -4,9 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 import java.io.IOException;
